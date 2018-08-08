4b0b2b05317704719a1029cd6d1373b2bb71aee3
elun1 <elias.lundmark@outlook.com>
2018-08-08 23:30:45 +0200

    first commit

---
+++ azurebackend.py
@@ -0,0 +1,98 @@
+import requests
+from azure.common.credentials import UserPassCredentials
+
+'''
+Requires the azure python SDK. Can be a hassle to install, but works with pip3 version 18.0
+pip3 install azure
+'''
+
+class AzureInfo(object):
+
+
+    def __init__(self, resource_group, subID, access_token, appservice_name=False):
+
+        self.resource_group = resource_group
+        self.subID = subID
+        self.access_token = access_token
+        if appservice_name == True:
+            self.appservice_name = appservice_name
+        else:
+            self.appservice_name = self.resource_group
+        self.get_instance_info()
+        self.instance_count = len(self.instances)
+        self.get_hostnames()
+
+    def get_instance_info(self):
+
+        self.arraffinity_values = []
+        self.instances = {}
+
+        azureURL = 'https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Web/sites/{}/instances?api-version=2017-04-01'.format(
+                   self.subID,
+                   self.resource_group,
+                   self.appservice_name)
+
+        r = requests.get(azureURL, headers={'Authorization':self.access_token})
+
+        data = r.json()
+        data = data['value']
+
+        for x in data:
+            self.arraffinity_values.append(x['name'])
+
+        kuduURL = 'https://{}.scm.azurewebsites.net/Env.cshtml'.format(self.appservice_name)
+
+        for arraffinity_value in self.arraffinity_values:
+            r = requests.get(kuduURL, headers={'Authorization':self.access_token}, cookies={'ARRAffinity':arraffinity_value})
+            data = str(r.content).split('\\r\\n')
+
+            for d in data:
+                if 'Machine name' in d:
+                    machine_name = d.split(' ')
+                    machine_name = machine_name[-1].replace('</li>', '')
+            
+            self.instances[machine_name] = arraffinity_value
+
+    def get_hostnames(self):
+
+        azureURL = 'https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Web/sites/{}/hostNameBindings?api-version=2018-02-01'.format(
+                   self.subID,
+                   self.resource_group,
+                   self.appservice_name)
+
+        r = requests.get(azureURL, headers={'Authorization':self.access_token})
+
+        data = r.json()
+        self.all_hostnames = []
+        self.custom_hostnames = []
+
+        for x in data['value']:
+            tmp_list = x['id'].split('/')
+            self.all_hostnames.append(tmp_list[-1])
+
+        for hostname in self.all_hostnames:
+            if 'azurewebsites' not in hostname and 'episerver' not in hostname:
+                self.custom_hostnames.append(hostname)
+
+def get_auth_token(username, passwd):
+
+    '''
+        returns access_token as a string, can be used as value for Authorization header
+    '''
+    credentials = UserPassCredentials(
+        username,
+        passwd
+    )
+
+    return credentials.token['token_type'] + ' ' + credentials.token['access_token']
+
+def test_instance_resptimes(hostname, instance_info, timeout=30):
+
+    resp_times = {}
+    for name, arraffinity_value in instance_info.items():
+        cookie = {'ARRAffinity':arraffinity_value}
+        time = requests.get('http://' + hostname, cookies=cookie, timeout=timeout).elapsed.total_seconds()
+        print('\tinstance ' + name + ' responded in ' + str(time) + ' seconds')
+        resp_times[name] = str(time)
+    
+    return resp_times
---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+from getpass import getpass
+from azurebackend import *
+
+def main():
+
+    subID = get_input('Subscription ID: ')
+    resource_group = get_input('Resource group name: ')
+    appservice_name = input('App Service name (empty if it\'s the same as resource group): ')
+    if appservice_name == '':
+        appservice_name = resource_group
+    username = get_input('Username: ')
+
+    token = get_auth_token(username, getpass())
+    prod_app = AzureInfo(resource_group, subID, token, appservice_name)
+
+    print('\n\nInformation for app service ' + prod_app.appservice_name + '\n')
+    print('Running instance count: ' + str(prod_app.instance_count))
+    for name, arr in prod_app.instances.items():
+        print('\t' + name + ' has ARRAffinity ' + arr)
+
+    print('\nAll hostnames:')
+    for hostname in prod_app.all_hostnames:
+        print('\t' + hostname)
+
+    print('\nCustom hostnames:')
+    for hostname in prod_app.custom_hostnames:
+        print('\t' + hostname)
+
+    print('\nTesting all instances on custom URL ' + prod_app.custom_hostnames[0])
+    resp_times = test_instance_resptimes(prod_app.custom_hostnames[0], prod_app.instances)
+
+def get_input(prompt):
+
+    result = False
+    while result == False:
+        result = input(prompt)
+        if not result:
+            result = False
+            print('\n Error: Empty input')
+
+    return result
+
+if __name__ == '__main__':
+    main()
